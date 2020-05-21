9b543ac43d3b2835cdc868f59a73c9504d96155b
Ululalbabalmujaddidy26 <65718059+Ululalbabalmujaddidy26@users.noreply.github.com>
2020-05-21 02:52:28 -1100

    Create jobset 1c++.cpp
    Buatlah  program array satudimensi yang menmapilkan masing masing nim mahasiswa

---
+++ jobset 1c++.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout<<"Ulul albab almujaddidy-F1B019139 KLP 26\n\n";
+int nim[5]= {9,8,7,9,13};
+	
+	cout << "Nim ke-1 = " << nim[0] << endl;
+	cout << "Nim ke-2 = " << nim[1] << endl;
+	cout << "Nim ke-3 = " << nim[2] << endl;
+	cout << "Nim ke-4 = " << nim[3] << endl;
+	cout << "Nim ke-5 = " << nim[4] << endl;
+
