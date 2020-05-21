68008a0c8057af7786c518e1c7916eed2933cd39
Ululalbabalmujaddidy26 <65718059+Ululalbabalmujaddidy26@users.noreply.github.com>
2020-05-21 02:47:25 -1100

    Create jobset1c.c
    Buatlah  program array satudimensi yang menmapilkan masing masing nim mahasiswa

---
+++ jobset1c.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+main (){
+	printf("Ulul albab almujaddidy F1B019139 KLP 26\n");
+	int nim[5]= {9,8,9,7,13};
+	
+	printf ("Nim ke-1 = %i\n",nim[0]);
+	printf ("Nim ke-2 = %i\n",nim[1]);
+	printf ("Nim ke-3 = %i\n",nim[2]);
+	printf ("Nim ke-4 = %i\n",nim[3]);
+	printf ("Nim ke-5 = %i\n",nim[4]);
+
+}
