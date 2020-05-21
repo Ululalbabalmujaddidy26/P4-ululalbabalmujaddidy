49c6147c44dae894f43e1eae7256172e52df697a
Ululalbabalmujaddidy26 <65718059+Ululalbabalmujaddidy26@users.noreply.github.com>
2020-05-21 02:53:43 -1100

    Create jobset 2c.c
    Buatlah program mengisi dan menampilkan elemen array dengan index array sesuai dengan  NIM terakhir setiap mahasiswa

---
+++ jobset 2c.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+main (){
+	printf("Ulul albab almujaddidy F1B019139 KLP 26\n");
+	int nim[5];
+printf ("Masukkan nim ke-1 ");
+	scanf ("%i", &nim[0]);
+printf ("Masukkan nim ke-2 ");
+	scanf ("%i", &nim[1]);
+printf ("Masukkan nim ke-3 ");
+	scanf ("%i", &nim[2]);
+printf ("Masukkan nim ke-4 ");
+	scanf ("%i", &nim[3]);
+printf ("Masukkan nim ke-5 ");
+	scanf ("%i", &nim[4]);
+	printf("%i nim ke-1",nim[0]);
+	printf("%i nim ke-2",nim[1]);
+	printf("%i nim ke-3",nim[2]);
+	printf("%i nim ke-4",nim[3]);
+	printf("%i nim ke-5",nim[4]);
+
+	}
