d31763052a955ae8338198b8fde1671d2f5a528e
Ululalbabalmujaddidy26 <65718059+Ululalbabalmujaddidy26@users.noreply.github.com>
2020-05-21 02:58:39 -1100

    Create jobset c++.cpp
    Buatlah program mengisi dan menampilkan elemen array dengan index array sesuai dengan  NIM terakhir setiap mahasiswa

---
+++ jobset c++.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+main (){
+	cout<<"Ulul albab almujaddidy F1B019139 KLP 26\n";
+	int nim[5];
+	cout << "Masukkan nim ke-1 ";
+	cin >> nim[0];
+cout << "Masukkan nim ke-2 ";
+	cin >> nim[1];
+cout << "Masukkan nim ke-3 ";
+	cin >> nim[2];
+cout << "Masukkan nim ke-4 ";
+	cin >> nim[3];
+cout << "Masukkan nim ke-5 ";
+	cin >> nim[4];
+cout<<"nim ke-1 = "<<nim[0]<<endl;
+cout<<"nim ke-2 = "<<nim[1]<<endl;
+cout<<"nim ke-3 = "<<nim[2]<<endl;
+cout<<"nim ke-4 = "<<nim[3]<<endl;
+cout<<"nim ke-5 = "<<nim[4]<<endl;
+
+}
